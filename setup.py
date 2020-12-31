60d31dd9729364b5a3e1aa3d92701d40856bc53f
Barry <barry.cahill@gmail.com>
2020-12-31 11:50:34 +0100

    2

---
+++ setup.py
@@ -0,0 +1,15 @@
+#setup.py
+
+from setuptools import setup, find_packages
+
+setup(name='cb_diag',
+    version='0.0.1',
+    description='cb_diag_tools',
+    author='barry',
+    url='https://github.com/barrycahill/cb_diag',
+    packages=['cb_diag'],
+    install_requires=[
+        #'unidecode',
+        #'awesome-slugify'
+    ]
+)
